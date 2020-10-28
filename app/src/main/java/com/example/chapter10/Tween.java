608bcc18541eec70a7391472d137442684c644b3
reemk8 <72727099+reemk8@users.noreply.github.com>
2020-10-28 11:43:17 +0300

    Initial commit

---
+++ app/src/main/java/com/example/chapter10/Tween.java
@@ -0,0 +1,19 @@
+package com.example.chapter10;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class Tween extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate ( savedInstanceState );
+        setContentView ( R.layout.activity_tween );
+        ImageView imgRotate=(ImageView) findViewById ( R.id.imgTween );
+        imgRotate.startAnimation ( AnimationUtils.loadAnimation ( this,R.anim.rotation ) );
+    }
+}
